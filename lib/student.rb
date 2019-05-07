2371cf6e3fa214e5fb8299aa2722ddc23f49bb5d
Barbara Johnson <johnson.barbara@gmail.com>
2019-05-07 01:31:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -51,11 +51,11 @@ class Student
         sql = <<-SQL
       SELECT *
       FROM students
-      WHERE grade = 12
-      LIMIT x;
+      WHERE grade = 10
+      LIMIT ?
     SQL
  
-    DB[:conn].execute(sql).map do |row|
+    DB[:conn].execute(sql, x).map do |row|
       self.new_from_db(row)
     end
   end 
